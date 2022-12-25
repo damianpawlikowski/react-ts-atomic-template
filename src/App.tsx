f8083442c7b9f088b5c92fc9c898f838756d10d9
Damian Pawlikowski <51343900+damianpawlikowski@users.noreply.github.com>
2022-12-25 15:52:26 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { FunctionComponent } from 'react'
+import { BrowserRouter, Routes } from 'react-router-dom'
+
+const App: FunctionComponent = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* Routes */}
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
